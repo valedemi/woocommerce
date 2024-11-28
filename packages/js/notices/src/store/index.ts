85ce9b33578b92349441411c1bcf061551488072
rjchow <me@rjchow.com>
2024-11-28 08:59:34 +0800

    wp-6.6

--- packages/js/notices/src/store/index.ts
+++ packages/js/notices/src/store/index.ts
@@ -17,6 +17,7 @@ export * from './types';
 export const STORE_NAME = 'core/notices2';
 // NOTE: This uses core/notices2, if this file is copied back upstream
 // to Gutenberg this needs to be changed back to core/notices.
+// @ts-expect-error - registerStore is not typed
 export default registerStore< State >( STORE_NAME, {
 	reducer: reducer as Reducer< State, AnyAction >,
 	actions,
