9a01c51c58af8b72807bdc9f8696df245a7bd79d
rjchow <me@rjchow.com>
2024-11-28 08:59:46 +0800

    wip

--- packages/js/data/src/countries/index.ts
+++ packages/js/data/src/countries/index.ts
@@ -14,11 +14,11 @@ import * as actions from './actions';
 import * as resolvers from './resolvers';
 import reducer, { State } from './reducer';
 import { WPDataSelectors } from '../types';
-import { PromiseifySelectors } from '../types/promiseify-selectors';
+// import { PromiseifySelectors } from '../types/promiseify-selectors';
 export * from './types';
 export type { State };
 
-registerStore< State >( STORE_NAME, {
+registerStore( STORE_NAME, {
 	reducer: reducer as Reducer< State, AnyAction >,
 	actions,
 	controls,
@@ -36,7 +36,7 @@ declare module '@wordpress/data' {
 	function select(
 		key: typeof STORE_NAME
 	): SelectFromMap< typeof selectors > & WPDataSelectors;
-	function resolveSelect(
-		key: typeof STORE_NAME
-	): PromiseifySelectors< SelectFromMap< typeof selectors > >;
+	// function resolveSelect(
+	// 	key: typeof STORE_NAME
+	// ): PromiseifySelectors< SelectFromMap< typeof selectors > >;
 }
